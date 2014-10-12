63acf4049215b84aaf55314f8f985de70539941a
justinvdm <justinvdm.0.1.0@gmail.com>
2014-10-12 21:39:19 +0200

    implement the basics

--- sig.js
+++ sig.js
@@ -1,6 +1,76 @@
 ;(function() {
-  function sig() {
+  sig.reset = reset
+  sig.push = push
+  sig.receive = receive
+  sig.watch = watch
+  sig.unwatch = unwatch
+
+
+  function sig(receiver) {
+    return {
+      type: 'sig',
+      sources: [],
+      targets: [],
+      receiver: receiver || noop
+    }
   }
+
+
+  function reset(s) {
+    s.sources.map(function(source) { untarget(source, s) })
+    s.targets.map(function(target) { unsource(s, target) })
+    s.sources = []
+    s.targets = []
+    return s
+  }
+
+
+  function watch(s, t) {
+    s.targets.push(t)
+    t.sources.push(s)
+  }
+
+
+  function untarget(s, t) {
+    rm(s.targets, t)
+  }
+
+
+  function unsource(s, t) {
+    rm(t.sources, s)
+  }
+
+
+  function unwatch(s, t) {
+    unsource(s, t)
+    untarget(s, t)
+  }
+
+
+  function push(s, x) {
+    var targets = s.targets
+    var i = -1
+    var n = targets.length
+    while (++i < n) receive(targets[i], x)
+    return s
+  }
+
+
+  function receive(s, x) {
+    s.receiver(x, s)
+    return s
+  }
+
+
+  function rm(arr, x) {
+    var i = arr.indexOf(x)
+    if (i < 0) return
+    arr.splice(i, 1)
+  }
+
+
+  function noop() {}
+
 
   if (typeof module != 'undefined') {
     module.exports = sig
--- sig.test.js
+++ sig.test.js
@@ -1,2 +1,90 @@
+require('chai').should()
+var sig = require('./sig')
+
+
 describe("sig", function() {
+  it("should allow multiple source signals", function() {
+    var results = []
+    var s1 = sig()
+    var s2 = sig()
+    var t = sig(function(x) { results.push(x) })
+
+    sig.watch(s1, t)
+    sig.watch(s2, t)
+
+    sig.push(s1, 1)
+    sig.push(s2, 2)
+    sig.push(s1, 3)
+    sig.push(s2, 4)
+    results.should.deep.equal([1, 2, 3, 4])
+  })
+
+  it("should allow multiple target signals", function() {
+    var results1 = []
+    var results2 = []
+    var s = sig()
+    var t1 = sig(function(x) { results1.push(x) })
+    var t2 = sig(function(x) { results2.push(x) })
+
+    sig.watch(s, t1)
+    sig.watch(s, t2)
+
+    sig.push(s, 1)
+    sig.push(s, 2)
+    sig.push(s, 3)
+    sig.push(s, 4)
+    results1.should.deep.equal([1, 2, 3, 4])
+    results2.should.deep.equal([1, 2, 3, 4])
+  })
+
+  it("should allow a target signal to be reset", function() {
+    var results = []
+    var s1 = sig()
+    var s2 = sig()
+    var t = sig(function(x) { results.push(x) })
+
+    sig.watch(s1, t)
+    sig.watch(s2, t)
+    sig.reset(t)
+
+    sig.push(s1, 1)
+    sig.push(s2, 2)
+    sig.push(s1, 3)
+    sig.push(s2, 4)
+    results.should.be.empty
+  })
+
+  it("should allow a source signal to be reset", function() {
+    var results1 = []
+    var results2 = []
+    var s = sig()
+    var t1 = sig(function(x) { results1.push(x) })
+    var t2 = sig(function(x) { results2.push(x) })
+
+    sig.watch(s, t1)
+    sig.watch(s, t2)
+    sig.reset(s)
+
+    sig.push(s, 1)
+    sig.push(s, 2)
+    sig.push(s, 3)
+    sig.push(s, 4)
+    results1.should.be.empty
+    results2.should.be.empty
+  })
+
+  it("should allow a signal to stop watching another", function() {
+    var results = []
+    var s = sig()
+    var t = sig(function(x) { results.push(x) })
+
+    sig.watch(s, t)
+    sig.unwatch(s, t)
+
+    sig.push(s, 1)
+    sig.push(s, 2)
+    sig.push(s, 3)
+    sig.push(s, 4)
+    results.should.be.empty
+  })
 })
