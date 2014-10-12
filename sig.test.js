93ef9cb09de9b4618248fcb2d1949835fbaecf50
justinvdm <justinvdm.0.1.0@gmail.com>
2014-10-12 22:08:17 +0200

    use drainpipe in tests

--- sig.test.js
+++ sig.test.js
@@ -1,5 +1,6 @@
 require('chai').should()
 var sig = require('./sig')
+var dp = require('drainpipe')
 
 
 describe("sig", function() {
@@ -16,6 +17,7 @@ describe("sig", function() {
     sig.push(s2, 2)
     sig.push(s1, 3)
     sig.push(s2, 4)
+
     results.should.deep.equal([1, 2, 3, 4])
   })
 
@@ -26,13 +28,16 @@ describe("sig", function() {
     var t1 = sig(function(x) { results1.push(x) })
     var t2 = sig(function(x) { results2.push(x) })
 
-    sig.watch(s, t1)
-    sig.watch(s, t2)
+    dp(s)
+      (sig.watch, t1)
+      (sig.watch, t2)
 
-    sig.push(s, 1)
-    sig.push(s, 2)
-    sig.push(s, 3)
-    sig.push(s, 4)
+    dp(s)
+      (sig.push, 1)
+      (sig.push, 2)
+      (sig.push, 3)
+      (sig.push, 4)
+
     results1.should.deep.equal([1, 2, 3, 4])
     results2.should.deep.equal([1, 2, 3, 4])
   })
@@ -51,6 +56,7 @@ describe("sig", function() {
     sig.push(s2, 2)
     sig.push(s1, 3)
     sig.push(s2, 4)
+
     results.should.be.empty
   })
 
@@ -65,10 +71,12 @@ describe("sig", function() {
     sig.watch(s, t2)
     sig.reset(s)
 
-    sig.push(s, 1)
-    sig.push(s, 2)
-    sig.push(s, 3)
-    sig.push(s, 4)
+    dp(s)
+      (sig.push, 1)
+      (sig.push, 2)
+      (sig.push, 3)
+      (sig.push, 4)
+
     results1.should.be.empty
     results2.should.be.empty
   })
@@ -81,43 +89,53 @@ describe("sig", function() {
     sig.watch(s, t)
     sig.unwatch(s, t)
 
-    sig.push(s, 1)
-    sig.push(s, 2)
-    sig.push(s, 3)
-    sig.push(s, 4)
+    dp(s)
+      (sig.push, 1)
+      (sig.push, 2)
+      (sig.push, 3)
+      (sig.push, 4)
+
     results.should.be.empty
   })
 
   it("should allow signals to map other signals", function() {
     var results = []
     var s = sig()
-    var dbl = sig.map(s, function(x) { return x * 2 })
-    var inc = sig.map(dbl, function(x) { return x + 1 })
-    sig.map(inc, function(x) { results.push(x) })
 
-    sig.push(s, 1)
-    sig.push(s, 2)
-    sig.push(s, 3)
-    sig.push(s, 4)
+    dp(s)
+      (sig.map, function(x) { return x * 2 })
+      (sig.map, function(x) { return x + 1 })
+      (sig.map, function(x) { results.push(x) })
+
+    dp(s)
+      (sig.push, 1)
+      (sig.push, 2)
+      (sig.push, 3)
+      (sig.push, 4)
+
     results.should.deep.equal([3, 5, 7, 9])
   })
 
   it("should allow signals to filter other signals", function() {
     var results = []
     var s = sig()
-    var odd = sig.filter(s, function(x) { return x % 2 })
-    var ltTen = sig.filter(odd, function(x) { return x < 10 })
-    sig.map(ltTen, function(x) { results.push(x) })
 
-    sig.push(s, 2)
-    sig.push(s, 3)
-    sig.push(s, 4)
-    sig.push(s, 5)
-    sig.push(s, 6)
-    sig.push(s, 11)
-    sig.push(s, 12)
-    sig.push(s, 15)
-    sig.push(s, 16)
+    dp(s)
+      (sig.filter, function(x) { return x % 2 })
+      (sig.filter, function(x) { return x < 10 })
+      (sig.map, function(x) { results.push(x) })
+
+    dp(s)
+      (sig.push, 2)
+      (sig.push, 3)
+      (sig.push, 4)
+      (sig.push, 5)
+      (sig.push, 6)
+      (sig.push, 11)
+      (sig.push, 12)
+      (sig.push, 15)
+      (sig.push, 16)
+
     results.should.deep.equal([3, 5])
   })
 })
