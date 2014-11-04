746153c5dc66515a9a40f29bb400ec62f3828bd0
justinvdm <justinvdm.0.1.0@gmail.com>
2014-11-04 08:49:44 +0200

    restructure tests

--- sig.test.js
+++ sig.test.js
@@ -162,150 +162,6 @@ describe("sig", function() {
     results.should.be.empty
   })
 
-  it("should allow signals to map other signals", function() {
-    var results = []
-    var s = sig()
-
-    v(s)
-     (sig.map, function(x) { return x * 2 })
-     (sig.map, function(x) { return x + 1 })
-     (sig.map, function(x) { results.push(x) })
-
-    v(s)
-     (sig.push, 1)
-     (sig.push, 2)
-     (sig.push, 3)
-     (sig.push, 4)
-
-    results.should.deep.equal([3, 5, 7, 9])
-  })
-
-  it("should allow signals to filter other signals", function() {
-    var s = sig()
-
-    var results = v(s)
-      (sig.filter, function(x) { return x % 2 })
-      (sig.filter, function(x) { return x < 10 })
-      (capture)
-      ()
-
-    v(s)
-     (sig.push, 2)
-     (sig.push, 3)
-     (sig.push, 4)
-     (sig.push, 5)
-     (sig.push, 6)
-     (sig.push, 11)
-     (sig.push, 12)
-     (sig.push, 15)
-     (sig.push, 16)
-
-    results.should.deep.equal([3, 5])
-  })
-
-  it("should provide the relevant stream to map functions", function(done) {
-    var s = sig()
-    var t = sig.map(s, function(x, u) {
-      u.should.equal(t)
-      done()
-    })
-    sig.push(s, 1)
-  })
-
-  it("should provide the relevant stream to filter functions", function(done) {
-    var s = sig()
-    var t = sig.filter(s, function(x, u) {
-      u.should.equal(t)
-      done()
-    })
-    sig.push(s, 1)
-  })
-
-  it("should support signal limiters", function() {
-    var results = []
-    var s = sig()
-
-    var results = v(s)
-      (sig.limit, 3)
-      (capture)
-      ()
-
-    v(s)
-     (sig.push, 1)
-     (sig.push, 2)
-     (sig.push, 3)
-     (sig.push, 4)
-     (sig.push, 5)
-     (sig.push, 6)
-
-    results.should.deep.equal([1, 2, 3])
-  })
-
-  it("should provide a 'once' limiter", function() {
-    var results = []
-    var s = sig()
-
-    var results = v(s)
-      (sig.once)
-      (capture)
-      ()
-
-    v(s)
-     (sig.push, 1)
-     (sig.push, 2)
-     (sig.push, 3)
-     (sig.push, 4)
-     (sig.push, 5)
-     (sig.push, 6)
-
-    results.should.deep.equal([1])
-  })
-
-  it("should provide a sugar method for mapping once", function() {
-    var results = []
-    var s = sig()
-
-    v(s)
-      (sig.then, function(x) { results.push(x) })
-
-    v(s)
-      (sig.push, 1)
-      (sig.push, 2)
-      (sig.push, 3)
-      (sig.push, 4)
-      (sig.push, 5)
-      (sig.push, 6)
-
-    results.should.deep.equal([1])
-  })
-
-  it("should prove a utility for checking if something is a signal", function() {
-    sig.isSig(void 0).should.be.false
-    sig.isSig(null).should.be.false
-    sig.isSig({}).should.be.false
-    sig.isSig(sig()).should.be.true
-  })
-
-  it("should provide a spread utility", function() {
-    v([1, 2, 3])
-     (sig.spread(function(a, b, c) {
-       return [a + 1, b + 1, c + 1]
-     }))
-     (sig.spread(function(a, b, c) {
-       return [a * 2, b * 2, c * 2]
-     }))
-     ()
-     .should.deep.equal([4, 6, 8])
-  })
-
-  it("s spread utility should append additional args", function() {
-    var fn = sig.spread(function(a, b, c, d) {
-      return [a, b, c, d]
-    })
-
-    fn([1, 2], 3, 4).should.deep.equal([1, 2, 3, 4])
-  })
-
   it("should support signal dependencies", function() {
     var s = sig()
     var t = sig()
@@ -383,5 +239,170 @@ describe("sig", function() {
   it("should act as an identity for existing signals", function() {
     var s = sig()
     sig(s).should.equal(s)
+  })
+})
+
+
+describe("sig.map", function() {
+  it("should map the given signal", function() {
+    var results = []
+    var s = sig()
+
+    v(s)
+     (sig.map, function(x) { return x * 2 })
+     (sig.map, function(x) { return x + 1 })
+     (sig.map, function(x) { results.push(x) })
+
+    v(s)
+     (sig.push, 1)
+     (sig.push, 2)
+     (sig.push, 3)
+     (sig.push, 4)
+
+    results.should.deep.equal([3, 5, 7, 9])
+  })
+
+  it("should provide the relevant stream to map functions", function(done) {
+    var s = sig()
+    var t = sig.map(s, function(x, u) {
+      u.should.equal(t)
+      done()
+    })
+    sig.push(s, 1)
+  })
+})
+
+
+describe("sig.filter", function() {
+  it("should filter the given signal", function() {
+    var s = sig()
+
+    var results = v(s)
+      (sig.filter, function(x) { return x % 2 })
+      (sig.filter, function(x) { return x < 10 })
+      (capture)
+      ()
+
+    v(s)
+     (sig.push, 2)
+     (sig.push, 3)
+     (sig.push, 4)
+     (sig.push, 5)
+     (sig.push, 6)
+     (sig.push, 11)
+     (sig.push, 12)
+     (sig.push, 15)
+     (sig.push, 16)
+
+    results.should.deep.equal([3, 5])
+  })
+
+  it("should provide the relevant stream to filter functions", function(done) {
+    var s = sig()
+    var t = sig.filter(s, function(x, u) {
+      u.should.equal(t)
+      done()
+    })
+    sig.push(s, 1)
+  })
+})
+
+
+describe("sig.limit", function() {
+  it("should limit the given signal", function() {
+    var results = []
+    var s = sig()
+
+    var results = v(s)
+      (sig.limit, 3)
+      (capture)
+      ()
+
+    v(s)
+     (sig.push, 1)
+     (sig.push, 2)
+     (sig.push, 3)
+     (sig.push, 4)
+     (sig.push, 5)
+     (sig.push, 6)
+
+    results.should.deep.equal([1, 2, 3])
+  })
+})
+
+
+describe("sig.once", function() {
+  it("should limit a signal to its first output", function() {
+    var results = []
+    var s = sig()
+
+    var results = v(s)
+      (sig.once)
+      (capture)
+      ()
+
+    v(s)
+     (sig.push, 1)
+     (sig.push, 2)
+     (sig.push, 3)
+     (sig.push, 4)
+     (sig.push, 5)
+     (sig.push, 6)
+
+    results.should.deep.equal([1])
+  })
+})
+
+
+describe("sig.then", function() {
+  it("should only map a signal's first output", function() {
+    var results = []
+    var s = sig()
+
+    v(s)
+      (sig.then, function(x) { results.push(x) })
+
+    v(s)
+      (sig.push, 1)
+      (sig.push, 2)
+      (sig.push, 3)
+      (sig.push, 4)
+      (sig.push, 5)
+      (sig.push, 6)
+
+    results.should.deep.equal([1])
+  })
+})
+
+
+describe("sig.isSig", function() {
+  it("should determine whether something is a signal", function() {
+    sig.isSig(void 0).should.be.false
+    sig.isSig(null).should.be.false
+    sig.isSig({}).should.be.false
+    sig.isSig(sig()).should.be.true
+  })
+})
+
+
+describe("sig.spread", function() {
+  it("should spread an array out as a function's arguments", function() {
+    v([1, 2, 3])
+     (sig.spread(function(a, b, c) {
+       return [a + 1, b + 1, c + 1]
+     }))
+     (sig.spread(function(a, b, c) {
+       return [a * 2, b * 2, c * 2]
+     }))
+     ()
+     .should.deep.equal([4, 6, 8])
+  })
+
+  it("should append additional args", function() {
+    var fn = sig.spread(function(a, b, c, d) {
+      return [a, b, c, d]
+    })
+
+    fn([1, 2], 3, 4).should.deep.equal([1, 2, 3, 4])
   })
 })
